f5114b9f51cd568560a02a2eb7908a1e0a56b1ca
allikn0w <suurrend3r@gmail.com>
2018-10-29 21:21:23 -0300

    manejo de archivos

--- cgame.py
+++ cgame.py
@@ -5,14 +5,15 @@ import optparse
 from random import randint
 from random import choice
 from time import sleep
-'''
-class adf(object):
-  def __init__(self):
-    
 
-  def xx(self,,,):
-'''  
-  
+class Personaje(object):
+  def __init__(self,personaje,,,):
+    self.personaje = personaje
+
+  def Cargar_Personaje(self):
+    with open('personajes') as f:
+      lines = f.redlines()
+ 
 def main():
   parser = optparse.OptionParser()
   parser.add_option('-p','--personaje',
@@ -20,9 +21,14 @@ def main():
 
   options,args = parser.parse_args()
   print('Personaje elegido: ', options.personaje)
+  
 
-
-
+  print('[P]elear, [C]argar, [N]uevo personaje, [B]orrar, [S]alir');
+  
+  {'P':pelear(),
+   'C':cargar(),
+   'B':borrar(),
+   'N':crear()}[option]()
 
 
 
